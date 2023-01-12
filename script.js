c244592737360fa1028c79a3ed690ab3b21fa828
luanaigh17 <63931450+luanaigh17@users.noreply.github.com>
2023-01-11 21:57:09 -0600

    Last 3 uploads
    In the latest three uploads the buttons now work properly and it has a loading screen.

--- script.js
+++ script.js
@@ -3,14 +3,28 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 
 
 
 
 let apiQuotes = [];
+
+// Show loading
+function loading() {
+    loader.hidden=false;
+    quoteContainer.hidden = true;
+}
+
+//Hide loading
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden= true;
+}
 // show new quote
 function newQuote() {
+    loading();
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     // Checks for author
     if(quote.author==null) {
@@ -24,14 +38,15 @@ function newQuote() {
     }else {
         quoteText.classList.remove('long-quote');
     }
-
-
     quoteText.textContent=quote.text;
+    // hide loader
+    complete();
 }
 
 
 // Get Quotes from API
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     try {
         const response = await fetch(apiUrl);
@@ -42,6 +57,15 @@ async function getQuotes() {
     }
 }
 
+// tweet
+function tweetQuote() {
+    const twitterUrl= `https://twitter.com/intent/tweet?text=${quoteText.textcontent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // on Load
 getQuotes();
